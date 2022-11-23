61acf29146390b9f2c6ac55aa98a6ee4d1fdc550
Olivia Radick <olivia.radick@gmail.com>
2022-11-23 09:23:12 -0500

    commit tree 9cf0d8dab0fd313334fc2e873831538497a19691

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+
+newHeader.id = "victory"
+newHeader.textContent = "Olivia is the champion"
+
+// I want to practice adding and removing elements in the DOM. I also want to practice styling with js.
